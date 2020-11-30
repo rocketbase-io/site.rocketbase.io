2081e171213067f6066798a953fa1803eefbd387
Philipp Hiller <hiller.ph@gmail.com>
2020-11-30 16:37:53 +0100

    feat: updated yomei reference; example

--- src/_js/swiper.js
+++ src/_js/swiper.js
@@ -1,11 +1,12 @@
-import Swiper from 'swiper'
+import Swiper from 'swiper/bundle'
 
 var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 3,
   spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
