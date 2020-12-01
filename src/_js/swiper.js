6325d000bd5ba1704a042355b01c6e207d8de8b2
Philipp Hiller <hiller.ph@gmail.com>
2020-12-01 11:11:36 +0100

    feat: missing imports of swiper plugin; autoplay and pagination

--- src/_js/swiper.js
+++ src/_js/swiper.js
@@ -1,4 +1,7 @@
-import Swiper from 'swiper/bundle'
+import Swiper, { Pagination, Autoplay } from 'swiper';
+
+// configure Swiper to use modules
+Swiper.use([Pagination, Autoplay]);
 
 var swiper = new Swiper('.swiper-container', {
   spaceBetween: 30,
@@ -10,9 +13,5 @@ var swiper = new Swiper('.swiper-container', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  }
 });
